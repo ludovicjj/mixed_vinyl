82a68b93de544c191763f8da38b9c86c65eb5411
ludovicjj <jahanlud@gmail.com>
2022-10-11 15:55:40 +0200

    VinylController::browse: Autowiring VinylMixRepository directly

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\VinylMix;
+use App\Repository\VinylMixRepository;
 use App\Service\MixRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,7 +32,7 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(MixRepository $mixRepository, string $slug = null): Response
+    public function browse(VinylMixRepository $mixRepository, string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
         $mixes = $mixRepository->findAll();
