bcab1af764cffea82765c1629dcd57e6dc00d368
ludovicjj <jahanlud@gmail.com>
2022-10-13 12:13:30 +0200

    MixController::show: Use @ParamConverter to query VinylMix in show action

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\VinylMixRepository;
+use App\Entity\VinylMix;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +10,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class MixController extends AbstractController
 {
     #[Route('/mix/{id}', name: 'app_mix_show')]
-    public function show($id, VinylMixRepository $mixRepository): Response
+    public function show(VinylMix $mix): Response
     {
-        $mix = $mixRepository->find($id);
-
         return $this->render('mix/show.html.twig', [
            'mix' => $mix
         ]);
